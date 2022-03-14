6a305dcc80908ec8185842279553093dea4199bc
ijul <ijuldz@gmail.com>
2022-03-14 16:13:39 +0700

    add v-app component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
-import Vuetify from "vuetify/lib";
+import Vuetify, { VApp } from "vuetify/lib";
+import "vuetify/dist/vuetify.min.css";
 
 import KangButton from "./components/KangButton/KangButton.vue";
 
@@ -7,11 +8,13 @@ Vue.use(Vuetify);
 
 const Components = {
   KangButton,
+  VApp
 };
 
 Vue.component("KangButton", KangButton);
+Vue.component("KangApp", VApp);
 
-export { KangButton };
+export { Vapp as KangApp, KangButton };
 
 // Export the library as a plugin
 export default Components;
